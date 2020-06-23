12e828c7ced4293bfbc5decef0c0413b03d1349a
Nicholas Heo <nicholas.heo95@gmail.com>
2020-06-23 11:39:57 -0400

    basic idea done

--- App.js
+++ App.js
@@ -1,29 +1,36 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import {
+  Text,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  SafeAreaView,
+} from "react-native";
+import { NativeRouter, Route, Link } from "react-router-native";
+import Test from "./screens/test.js";
+import HomeScreen from "./screens/HomeScreen";
+import useCachedResources from "./hooks/useCachedResources";
 
 const Stack = createStackNavigator();
 
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
-
+  console.log("this is props ", props);
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
+      <NativeRouter>
+        <SafeAreaView>
+          {/* <Link to="/test">
+            <Text>To Test.js</Text>
+          </Link> */}
+          <Route path="/" component={HomeScreen}></Route>
+        </SafeAreaView>
+      </NativeRouter>
     );
   }
 }
@@ -31,6 +38,6 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
---
+++ screens/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Test() {
+  return (
+    <View>
+      <Text>All lives have been saved!</Text>
+    </View>
+  );
+}
